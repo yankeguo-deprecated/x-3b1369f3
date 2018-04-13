259910d10c49f82fdd5a783eb9835cc3a97d4877
Yanke Guo <guoyk.cn@gmail.com>
2018-04-14 00:16:46 +0800

    add runtime/binfs

---
+++ runtime/binfs/test/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"magi.systems/runtime/binfs"
+)
+
+func main() {
+	f, err := binfs.Open("/other/other2/hello.txt")
+	if err != nil {
+		panic(err)
+	}
+	s, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(s))
+	binfs.Walk(func(n *binfs.Node) {
+		var s string
+		if n.FileInfo().IsDir() {
+			s = "D"
+		} else {
+			s = "F"
+		}
+		fmt.Println("Node:", s, n.Path)
+	})
+}
